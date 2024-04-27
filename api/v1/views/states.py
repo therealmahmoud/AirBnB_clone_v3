3a58780fd055e5851e448fbd0a0a851af8273b4a
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-04-27 23:44:13 +0300

    “commit”

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ The index of the app."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -23,3 +23,42 @@ def states_id_get(id):
 	return clas
 
 
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def del_states():
+	clas = storage.get(State, id)
+	if clas is None:
+		raise 404
+	storage.delete(clas)
+	return {}
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_States():
+	dct = request.get_json()
+	if type(dct) != dict:
+		raise 400("Not a JSON")
+	if not dct["name"]:
+		raise 400("Missing name")
+	storage.new(dct)
+	storage.get(State,dct["id"])
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_states(id):
+	clas = storage.get(State, id)
+	if clas is None:
+		raise 404
+	dct = request.get_json()
+	if type(dct) != dict:
+		raise 400("Not a JSON")
+	for key, Value in dct.items():
+		if key is "id":
+			continue
+		if key is "created_at":
+			continue
+		if key is "updated_at":
+			continue
+		clas[key] = Value
+	storage.new(dct)
+	storage.get(State,dct["id"])
+	
