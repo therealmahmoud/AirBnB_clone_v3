de804a3466ac0ccd3972f197fcd5ee1ee8908b16
therealmahmoud~ <moha91875460@gmail.com>
2024-04-28 01:07:17 +0300

    updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ The index of the app."""
-from flask import Flask, jsonify, request
+from flask import jsonify, request, abort, make_response
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -9,56 +9,69 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models.base_model import BaseModel
+
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states_get():
-	return storage.all(State)
+    """ Retrieves the list of all State objects."""
+    dic = storage.all(State).values()
+    lis = []
+    for state in dic:
+        lis.append(state.to_dict())
+    return jsonify(lis)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def states_id_get(id):
-	clas = storage.get(State, id)
-	if clas is None:
-		raise 404
-	return clas
+def states_id_get(state_id):
+    """ Get an object."""
+    clas = storage.get(State, state_id)
+    if clas is None:
+        abort(404)
+    return jsonify(clas.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def del_states():
-	clas = storage.get(State, id)
-	if clas is None:
-		raise 404
-	storage.delete(clas)
-	return {}
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_states(state_id):
+    """ Delete an object."""
+    clas = storage.get(State, state_id)
+    if clas is None:
+        abort(404)
+    storage.delete(clas)
+    storage.save()
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_States():
-	dct = request.get_json()
-	if type(dct) != dict:
-		raise 400("Not a JSON")
-	if not dct["name"]:
-		raise 400("Missing name")
-	storage.new(dct)
-	storage.get(State,dct["id"])
+    """ Post an object."""
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    if "name" not in request.get_json():
+        abort(400, description="Missing name")
+    data = request.get_json()
+    obj = State(**data)
+    obj.save()
+    return make_response(jsonify(obj.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put_states(id):
-	clas = storage.get(State, id)
-	if clas is None:
-		raise 404
-	dct = request.get_json()
-	if type(dct) != dict:
-		raise 400("Not a JSON")
-	for key, Value in dct.items():
-		if key is "id":
-			continue
-		if key is "created_at":
-			continue
-		if key is "updated_at":
-			continue
-		clas[key] = Value
-	storage.new(dct)
-	storage.get(State,dct["id"])
-	
+def put_states(state_id):
+    """ Put or update an object."""
+    clas = storage.get(State, state_id)
+    if clas is None:
+        abort(404)
+    dct = request.get_json()
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    for key, value in dct.items():
+        if key is "id":
+            continue
+        if key is "created_at":
+            continue
+        if key is "updated_at":
+            continue
+        setattr(clas, key, value)
+    storage.save()
+    return make_response(jsonify(clas.to_dict()), 200)
