e22a80e37a52e30dbfa7a369c2d648413134be5d
therealmahmoud~ <moha91875460@gmail.com>
2024-04-27 07:47:14 +0300

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,16 @@
 """ The index of the app."""
 from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ Returns the status of the api."""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_count():
+    """ Retrieves the number of each objects by type."""
+    dic = {}
+    dic = storage.count()
+    return jsonify(dic)
