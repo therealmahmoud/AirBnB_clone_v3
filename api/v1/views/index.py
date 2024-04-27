44f24438e67048ea4db5f9a6323a8eb10910f764
therealmahmoud~ <moha91875460@gmail.com>
2024-04-27 07:39:29 +0300

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ Returns the status of the api."""
