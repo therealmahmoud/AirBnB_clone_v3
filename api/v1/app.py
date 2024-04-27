01c57e59af4cd9dd79f28e4c0347167b413aad8e
therealmahmoud~ <moha91875460@gmail.com>
2024-04-27 07:37:32 +0300

    updated

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Main module of an API!"""
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+from os import environ
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def stop(error):
+    """ Closes the db storage. """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host, port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" The index of the app."""
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """ Returns the status of the api."""
+    return jsonify({"status": "OK"})
