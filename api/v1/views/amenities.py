29e92b9878d4f1d7e9ddd24454ee6cdc2b5dd2a2
therealmahmoud~ <moha91875460@gmail.com>
2024-04-28 07:39:50 +0300

    updated

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -27,16 +27,21 @@ def amenity_get(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def del_amenity(amenity_id):
-    """ Delete an object."""
-    clas = storage.get(Amenity, amenity_id)
-    if not clas:
-        abort(404)
-    storage.delete(clas)
-    storage.save()
-    return make_response(jsonify({}), 200)
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenity():
+    """
+    Creates an amenity
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+    instance = Amenity(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
 
 
 @app_views.route('/amenities', methods=['POST'],
