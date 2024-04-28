2a2bac0dd414e5de4f8505789b27dc68099616fe
therealmahmoud~ <moha91875460@gmail.com>
2024-04-28 19:37:09 +0300

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ The index of the app."""
-from flask import Flask, jsonify
-from api.v1.views import app_views
-from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
