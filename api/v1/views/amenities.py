ec41d9c06b481246e3d7a79142d120580ec34e53
therealmahmoud~ <moha91875460@gmail.com>
2024-04-28 07:53:59 +0300

    updated

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,25 +6,26 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def get_amenities():
-    """
-    Retrieves a list of all amenities
-    """
-    all_amenities = storage.all(Amenity).values()
-    list_amenities = []
-    for amenity in all_amenities:
-        list_amenities.append(amenity.to_dict())
-    return jsonify(list_amenities)
-
-
-@app_views.route('/amenities/<amenity_id>/',
+@app_views.route('/amenities',
                  methods=['GET'], strict_slashes=False)
-def get_amenity_id(amenity_id):
-    """ Retrieves the list of all State objects."""
+def get_amenity():
+    """ Get an object."""
+    clas = storage.all(Amenity).values()
+    lis = []
+    for i in clas:
+        lis.append(i.to_dict())
+    return jsonify(lis)
+
+
+@app_views.route('/amenities/<amenity_id>/', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documentation/amenity/get_amenity.yml', methods=['GET'])
+def get_amenity(amenity_id):
+    """ Retrieves an amenity """
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
+
     return jsonify(amenity.to_dict())
 
 
