ac19c52a02dd3320f63d8ea805002c05eee0572b
therealmahmoud~ <moha91875460@gmail.com>
2024-04-28 03:42:03 +0300

    updated

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -44,36 +44,36 @@ def del_city(city_id):
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
 def post_city(state_id):
-    """ Post an object."""
+    """
+    Creates a City
+    """
     state = storage.get(State, state_id)
+    if not state:
+        abort(404)
     if not request.get_json():
         abort(400, description="Not a JSON")
-    if "name" not in request.get_json():
+    if 'name' not in request.get_json():
         abort(400, description="Missing name")
+
     data = request.get_json()
-    obj = City(**data)
-    obj.state_id = state.id
-    obj.save()
-    return make_response(jsonify(obj.to_dict()), 201)
+    instance = City(**data)
+    instance.state_id = state.id
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
-    """
-    Updates a City
-    """
-    city = storage.get(City, city_id)
-    if not city:
+    """ Put or update an object."""
+    clas = storage.get(City, city_id)
+    if not clas:
         abort(404)
-
     if not request.get_json():
         abort(400, description="Not a JSON")
-
-    ignore = ['id', 'state_id', 'created_at', 'updated_at']
-
-    data = request.get_json()
-    for key, value in data.items():
-        if key not in ignore:
-            setattr(city, key, value)
+    hash = ['id', 'state_id', 'created_at', 'updated_at']
+    dct = request.get_json()
+    for key, value in dct.items():
+        if key not in hash:
+            setattr(clas, key, value)
     storage.save()
-    return make_response(jsonify(city.to_dict()), 200)
+    return make_response(jsonify(clas.to_dict()), 200)
