13742a056b22163a56b123fd5e2470b87f599a7c
therealmahmoud~ <moha91875460@gmail.com>
2024-04-29 05:47:06 +0300

    updated

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ objects that handles all default RestFul API actions for review """
+from models.review import Review
+from models.place import Place
+from models.user import User
 from flask import jsonify, request, abort, make_response
 from api.v1.views import app_views
 from models import storage
-from models.review import Review
+
 
 
 @app_views.route('/places/<place_id>/reviews',
