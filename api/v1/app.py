e8efba3381d31502045c6055de42000a47793c0b
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-04-30 01:02:48 +0300

    “commit”

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
