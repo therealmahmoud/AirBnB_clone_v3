8328d49d6a8c6da556e6adae0447235ef6a72c02
therealmahmoud~ <moha91875460@gmail.com>
2024-05-02 06:13:10 +0300

    updated

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,21 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """
+        Overrides the default setting of an attribute.
+        Specifically, it encrypts the password before setting it.
+
+        Parameters:
+        - name (str): The name of the attribute to be set.
+        - value: The value to be assigned to the attribute.
+        If the attribute name is 'password',
+        this value will be encrypted using MD5.
+
+        Returns:
+        None
+        """
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
